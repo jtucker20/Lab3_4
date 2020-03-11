153cbd554bde5ea144075a3692b6b8acf9f8aa52
Wizard of Love <andreberrya@gmail.com>
2020-03-11 19:36:08 -0400

    Update

--- src/main/java/edu/mcdaniel/java2206/AndreBerry_lab3_4/fords/FordF150.java
+++ src/main/java/edu/mcdaniel/java2206/AndreBerry_lab3_4/fords/FordF150.java
@@ -2,17 +2,26 @@ package edu.mcdaniel.java2206.AndreBerry_lab3_4.fords;
 
 import edu.mcdaniel.java2206.AndreBerry_lab3_4.car.VehicleAbstractClass;
 import edu.mcdaniel.java2206.AndreBerry_lab3_4.interfaces.Vehicle;
+import edu.mcdaniel.java2206.AndreBerry_lab3_4.interfaces.WifiEnabledVehicle;
+import edu.mcdaniel.java2206.AndreBerry_lab3_4.security.*;
+import edu.mcdaniel.java2206.AndreBerry_lab3_4.fords.FordF150;
+import edu.mcdaniel.java2206.AndreBerry_lab3_4.vehicles.FordVehicle;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class FordF150 extends VehicleAbstractClass implements Vehicle {
+public class FordF150 extends VehicleAbstractClass implements Vehicle, WifiEnabledVehicle {
 
     //Private Assets
+    private Logger log = LogManager.getLogger(FordF150.class);
+
     private double accelerationRate;
     private double distance;
+    private String fordWifiPassword;
 
     private boolean lightsOn;
 
     //Constructors
-    public FordF150(){
+    public FordF150() {
         super();
         setModel("F150");
         this.lightsOn = false;
@@ -46,13 +55,31 @@ public class FordF150 extends VehicleAbstractClass implements Vehicle {
 
     @Override
     public String showLightsStatus() {
-        return "The Lights are " + ( (this.lightsOn) ? "On" : "Off");
+        return "The Lights are " + ((this.lightsOn) ? "On" : "Off");
     }
 
     @Override
     public String showVehicleState() {
         return "This Ford F150 has an acceleration of: " + this.accelerationRate
                 + "\nThis Ford F150 has it's lights " + ((this.lightsOn) ? "On" : "Off");
+    }
+
+    @Override
+    public String getWifiProvider() {
+        return WifiProviderName.FORD_VEHICLE_WIFI_PROVIDER.toString();
+    }
+
+    @Override
+    public String getNetworkName() {
+        //a lazily created network name most likely implemented by someone with a lack of imagination.
+        return "labNetwork";
+    }
+
+    @Override
+    public Token getToken() {
+        return TokenFactory.getToken(this.getWifiProvider(),
+                this.getNetworkName(),
+                this.fordWifiPassword);
     }
 
     //Getters
@@ -67,4 +94,11 @@ public class FordF150 extends VehicleAbstractClass implements Vehicle {
     public boolean isLightsOn() {
         return lightsOn;
     }
+
+    @Override
+    public void setWifiPassword(String password) {
+        this.fordWifiPassword = password;
+
+    }
+
 }
